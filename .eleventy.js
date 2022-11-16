1e335b56ff3591e1eac441587a09843cb6696f68
Michael Joseph Miller <michael.joseph.millee@gmail.com>
2022-11-16 14:19:15 -0500

    converted to ejs

--- .eleventy.js
+++ .eleventy.js
@@ -1,40 +1,44 @@
-const filters = require('./utils/filters.js')
-const transforms = require('./utils/transforms.js')
-const collections = require('./utils/collections.js')
+const filters = require("./utils/filters.js");
+const transforms = require("./utils/transforms.js");
+const collections = require("./utils/collections.js");
 
 module.exports = function (eleventyConfig) {
 	// Folders to copy to build dir (See. 1.1)
 	eleventyConfig.addPassthroughCopy("src/static");
 
-	// Filters 
+	// Filters
 	Object.keys(filters).forEach((filterName) => {
-		eleventyConfig.addFilter(filterName, filters[filterName])
-	})
+		eleventyConfig.addFilter(filterName, filters[filterName]);
+	});
 
 	// Transforms
 	Object.keys(transforms).forEach((transformName) => {
-		eleventyConfig.addTransform(transformName, transforms[transformName])
-	})
+		eleventyConfig.addTransform(transformName, transforms[transformName]);
+	});
 
 	// Collections
 	Object.keys(collections).forEach((collectionName) => {
-		eleventyConfig.addCollection(collectionName, collections[collectionName])
-	})
+		eleventyConfig.addCollection(collectionName, collections[collectionName]);
+	});
 
 	// This allows Eleventy to watch for file changes during local development.
 	eleventyConfig.setUseGitIgnore(false);
+
+	// This allows access to filters in ejs templates using the js global var
+	// usage: js.filterName(varToFilter)
+	js = eleventyConfig.javascriptFunctions;
 
 	return {
 		dir: {
 			input: "src/",
 			output: "dist",
 			includes: "_includes",
-			layouts: "_layouts"
+			layouts: "_layouts",
 		},
-		templateFormats: ["html", "md", "njk"],
-		htmlTemplateEngine: "njk",
+		templateFormats: ["html", "md", "ejs"],
+		htmlTemplateEngine: "ejs",
 
 		// 1.1 Enable eleventy to pass dirs specified above
-		passthroughFileCopy: true
+		passthroughFileCopy: true,
 	};
 };
